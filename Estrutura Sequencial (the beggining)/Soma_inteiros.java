9109a7f7a8faba2205bf5e3755b593b1ec0d976c
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-05-21 23:59:22 -0300

    Update Soma_inteiros.java

--- Estrutura Sequencial (the beggining)/Soma_inteiros.java
+++ Estrutura Sequencial (the beggining)/Soma_inteiros.java
@@ -5,12 +5,15 @@ public class Main
 	public static void main(String[] args) {
 		
 		int a, b, soma;
-		
+		//learning scanner mode on java
 		Scanner sc = new Scanner(System.in);
+
 		System.out.println("digite o primeiro numero: ");
 		a = sc.nextInt();
+
 		System.out.println("digite o primeiro numero: ");
 		b = sc.nextInt();
+
 		soma = a+b;
 		
 		System.out.println("SOMA = "+soma);
