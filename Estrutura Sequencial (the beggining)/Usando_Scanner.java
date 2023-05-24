64a9967b5f73bb29741302b4f30a814e9c42b919
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-05-24 00:06:17 -0300

    Update Usando_Scanner.java

--- Estrutura Sequencial (the beggining)/Usando_Scanner.java
+++ Estrutura Sequencial (the beggining)/Usando_Scanner.java
@@ -6,12 +6,18 @@ public class Usando_Scanner {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		//String x;
-		int x;
-		x = sc.nextInt();
+		String x;
+		int y;
+		double z;
 		
-		System.out.println("Você digitou: "+x);
+		x = sc.next();
+		y = sc.nextInt();
+		z = sc.nextDouble();
 		
+		System.out.println("Você digitou: ");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
 		sc.close();
 	}
 
