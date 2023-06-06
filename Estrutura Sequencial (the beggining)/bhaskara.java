8f2f64717ad5e9059b8068c162b71d12ded35800
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-05 23:57:17 -0300

    Update bhaskara.java

--- Estrutura Sequencial (the beggining)/bhaskara.java
+++ Estrutura Sequencial (the beggining)/bhaskara.java
@@ -6,7 +6,7 @@ public class bhaskara {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double a, b, c, delta;
+		double a, b, c, delta, x1, x2;
 		
 		Scanner scan = new Scanner(System.in);
 		
@@ -16,9 +16,11 @@ public class bhaskara {
 
 		delta  = Math.pow(b, 2) - 4 * a * c;
 		
-		x1 = 
-		x2 = 
+		x1 = (- b + Math.sqrt(delta))/(2*a);
+		x2 = (- b - Math.sqrt(delta))/(2*a);
 		
+		System.out.println("x1: "+x1);
+		System.out.println("x1: "+x2);
 	}
 
 }
