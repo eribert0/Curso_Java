bf631732b65ef2530a560bd5c9232ef686383615
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-05 23:53:19 -0300

    Create bhaskara.java

---
+++ Estrutura Sequencial (the beggining)/bhaskara.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class bhaskara {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a, b, c, delta;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		a = scan.nextDouble();
+		b = scan.nextDouble();
+		c = scan.nextDouble();
+
+		delta  = Math.pow(b, 2) - 4 * a * c;
+		
+		x1 = 
+		x2 = 
+		
+	}
+
+}
