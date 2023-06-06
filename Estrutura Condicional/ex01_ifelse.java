f3276c67f14191d0175375320f0a5b0f8f20cea3
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-06 19:40:51 -0300

    Create ex01_ifelse.java

---
+++ Estrutura Condicional/ex01_ifelse.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class exercicio_ifelse {
+
+	public static void main(String[] args) {
+		double x, y;
+		
+		Scanner sc  = new Scanner(System.in);
+		
+		System.out.println("digite o valor de x: ");
+		x = sc.nextDouble();
+		
+		System.out.println("digite o valor de y: ");
+		y = sc.nextDouble();
+		
+		if( x == 0.0 && y == 0.0) {
+			System.out.println("Origem");
+		} else if(x > 0.0 && y > 0.0){
+			System.out.println("Q1");
+		} else if(x < 0.0 && y > 0.0){
+			System.out.println("Q2");
+		} else if(x < 0.0 && y < 0.0){
+			System.out.println("Q3");
+		} else if(x > 0.0 && y < 0.0){
+			System.out.println("Q4");
+		}
+		
+		sc.close();
+	}
+
+}
