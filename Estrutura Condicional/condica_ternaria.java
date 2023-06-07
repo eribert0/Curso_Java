74d524163788270669fbc34f2028f5244eb7792f
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-06 23:41:03 -0300

    Update condica_ternaria.java

--- Estrutura Condicional/condica_ternaria.java
+++ Estrutura Condicional/condica_ternaria.java
@@ -2,8 +2,9 @@
 public class condicional_ternaria {
 
 	public static void main(String[] args) {
-		double preco = 34.5;
-		double desconto = (preco < 20) ? preco*0.1 : preco *0.05;	
+		
+        double preco = 34.5;
+		double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;	
 		
 		/*if(preco < 20.00){
 			desconto = preco * 0.1;
