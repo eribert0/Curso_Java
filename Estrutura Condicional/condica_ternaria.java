71a4cf01d229c50e339be59f59fa984756801d79
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-06 23:40:03 -0300

    Create condica_ternaria.java

---
+++ Estrutura Condicional/condica_ternaria.java
@@ -0,0 +1,17 @@
+
+public class condicional_ternaria {
+
+	public static void main(String[] args) {
+		double preco = 34.5;
+		double desconto = (preco < 20) ? preco*0.1 : preco *0.05;	
+		
+		/*if(preco < 20.00){
+			desconto = preco * 0.1;
+		} else {
+			desconto = preco * 0.05;
+		}*/
+		
+		System.out.println("valor do desconto: "+desconto);
+	}
+
+}
