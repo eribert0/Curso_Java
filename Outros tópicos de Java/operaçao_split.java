2dfe474923531d473813506fdcda96df76d18b48
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-15 00:12:05 -0300

    Create operaçao_split.java

---
+++ Outros tópicos de Java/operaçao_split.java
@@ -0,0 +1,17 @@
+
+public class operaçao_split {
+
+	public static void main(String[] args) {
+		
+		String s = "Corinthians gigantesco campeão de tudo";
+		
+		String[] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		System.out.println(vect[3]);
+		System.out.println(vect[4]);	
+	}
+
+}
