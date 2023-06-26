44dd87ca106c3b0d5f23befe4570e58f2a5b3e2c
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-26 10:22:29 -0300

    Create Program.java

---
+++ Introdução_POO/course_Ex_membros_estaticos/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.print("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = Calculator.circumference(radius);
+		
+		double v = Calculator.volume(radius);
+		
+		System.out.printf("circumference: %.2f\n", c);
+		System.out.printf("volume: %.2f\n", v);
+		System.out.printf("value PI: %.2f\n", Calculator.PI);
+		
+		sc.close();
+	}
+	
+	
+}
