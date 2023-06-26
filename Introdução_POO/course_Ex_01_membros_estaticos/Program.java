eb98091dd65a9f12345fc5a5506f5ccf234d9c8e
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-26 15:06:47 -0300

    Update Program.java

--- Introdução_POO/course_Ex_01_membros_estaticos/Program.java
+++ Introdução_POO/course_Ex_01_membros_estaticos/Program.java
@@ -3,16 +3,23 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import util.CurrencyConverter;
+
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
 		
-		System.out.print("Enter dólar quotation: ");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("What's the dollar price? ");
 		double quotation = sc.nextDouble();
 		
+		System.out.print("How many dollars will be bought? ");
+		double dollar = sc.nextDouble();
 		
+		System.out.printf("Amount to be paid in reais = %.2f\n", 
+				+ CurrencyConverter.finalPrice(quotation, dollar));
 		
 		sc.close();
 	}
