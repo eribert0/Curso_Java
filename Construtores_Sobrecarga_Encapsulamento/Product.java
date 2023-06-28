ee1f9d0da076b111a791fd27d416734551ec0481
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-06-28 18:34:31 -0300

    Update Product.java

--- Construtores_Sobrecarga_Encapsulamento/Product.java
+++ Construtores_Sobrecarga_Encapsulamento/Product.java
@@ -1,16 +1,50 @@
 package entities;
 
-public class Product {
+public class Product { //PROPOSTA DE MELHORIA 
 	public String name;
 	public double price;
 	public int quantity;
-
+	//Construtores geralmente são depois dos atributos e antes dos métodos
+	
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
 	}
 	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String toString() {
+		return name
+			+ ", $"
+			+ String.format("%.2f", price)
+			+ ", "
+			+ quantity
+			+ " units, Total: $ "
+			+ String.format("%.2f", totalValueInStock())
+			+ "\n";
+	}	
+}
+
+
+/*package entities;
+
+public class Product {
+
+	public String name; // usando camel case
+	public double price;
+	public int quantity;
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}
@@ -22,12 +56,16 @@ public class Product {
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
-
+	
 	public String toString() {
-		return name + ", $ " +
-				String.format("%.2f", price) + 
-				", " + quantity + 
-				" units, Total: $ "	+ 
-				String.format("%.2f", totalValueInStock());
+		return name 
+			+ ", $ "
+			+ String.format("%.2f", price)
+			+ ", "
+			+ quantity
+			+ " units, Total: $ "
+			+ String.format("%.2f", totalValueInStock());
 	}
+	
 }
+*/
